2bc73270d9fa9788ae2a3f5c221cdb3d413a15d4
Jean Ibarz <ibarz.jean@gmail.com>
2015-09-14 23:15:18 +0200

    Project starting
    Starting project with a design of the main program window.

---
+++ src/free/fir/filter/generator/FreeFIRFilterGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package free.fir.filter.generator;
+
+/**
+ *
+ * @author Jean
+ */
+public class FreeFIRFilterGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FreeFirFilterGeneratorForm mainForm = new FreeFirFilterGeneratorForm();
+        mainForm.setVisible(true);
+    }
+    
+}
