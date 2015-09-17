52137b29496d326fbf8a6762930683ad34f3a0d0
Jean Ibarz <ibarz.jean@gmail.com>
2015-09-17 11:52:28 +0200

    Changed project file structure.

--- src/FreeFirFilterGenerator/FreeFirFilterGeneratorForm.java
+++ src/FreeFirFilterGenerator/FreeFirFilterGeneratorForm.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package free.fir.filter.generator;
+package freefirfiltergenerator;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
